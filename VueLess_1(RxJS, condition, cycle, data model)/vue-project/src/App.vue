fbe7f0431d9b582db286f0f76395326dba5c5580
ray3en <tigranlee@gmail.com>
2024-01-19 18:44:34 +0300

    init

---
+++ VueLess_1(RxJS, condition, cycle, data model)/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2 class="item">Hello world!</h2>
+  <!-- 
+  <Block/>
+  <ContentList/> 
+  -->
+  <Cycle></Cycle>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import ContentList from './components/ContentList.vue'
+import Cycle from './components/Cycle.vue'
+
+export default {
+  components: {
+    Block, ContentList, Cycle
+  }
+}
+</script>
+
+<style scoped>
+  .item{
+    color: red;
+  }
+</style>
---
+++ VueLess_2()/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <img src="./assets/kitty1.png" />
+</template>
+
+<style scoped>
+@import url('....google.fonts');
+
+header {
+  background-image: url('./assets/kitty1.png');
+}
+</style>
